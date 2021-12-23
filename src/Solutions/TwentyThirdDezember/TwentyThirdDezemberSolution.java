2fbd888c80458b618e6f70c13c2e693ac05f8837
ferdinands <ferdistr08@gmail.com>
2021-12-23 09:39:27 +0100

    23. Dezember

--- src/Solutions/TwentyThirdDezember/TwentyThirdDezemberSolution.java
+++ src/Solutions/TwentyThirdDezember/TwentyThirdDezemberSolution.java
@@ -2,6 +2,7 @@ package Solutions.TwentyThirdDezember;
 
 public class TwentyThirdDezemberSolution {
     public void Solution(){
+        //Solving by Hand
         int partOne = 13455;
         int partTwo = 43567;
         System.out.println("The Solution is/ Part 1: " + partOne + " Part 2: " + partTwo );
